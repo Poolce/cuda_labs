e39d2fb788f5059a0099ec3ccafbc2f34a49bd65
Alexandr <96471504+Poolce@users.noreply.github.com>
2023-10-24 15:49:14 +0300

    Added 2 random tests

--- lab1_taxpy/main.cpp
+++ lab1_taxpy/main.cpp
@@ -1,7 +1,7 @@
+#include <random>
 #include "taxpy_interface.h"
 #include "cuda_taxpy_template.h"
 #include "test.h"
-
 
 void Run_tests(){
     test((char*)"FLOAT CPU AND GPU 100 ELEMENTS", [](){
@@ -45,7 +45,23 @@ void Run_tests(){
         delete[] copyB;
         return is_equal(vec_size, vecB, copyB);
     });
+    test((char*)"FLOAT CPU OMP4 AND GPU RANDOM PARAMETERS", [](){
+        //init
+        omp_set_num_threads(4);
+        int vec_size = rand()%1800+200;
+        float* vecA = get_rand_vector<float>(vec_size);
+        float* vecB = get_rand_vector<float>(vec_size);
 
+        //run
+        float* copyB = copy_vector(vec_size, vecB);
+        //GPU res in vecB
+        cuda_t_axpy<float>(vec_size, vecA, rand()%20+5, vecB, rand()%20+5, 4.53);
+
+        //CPU res in copyB
+        taxpy<float>(vec_size, vecA, rand()%20+5, copyB, rand()%20+5, 4.53);
+
+        return is_equal(vec_size, vecB, copyB);
+    });
     test((char*)"DOUBLE CPU AND GPU 100 ELEMENTS", [](){
         //init
         omp_set_num_threads(1);
@@ -67,7 +83,7 @@ void Run_tests(){
         return is_equal(vec_size, vecB, copyB);
     });
     
-    test((char*)"FLOAT CPU OMP4 AND GPU 1000 ELEMENTS", [](){
+    test((char*)"DOUBLE CPU OMP4 AND GPU 1000 ELEMENTS", [](){
         //init
         omp_set_num_threads(4);
         int vec_size = 1000;
@@ -85,6 +101,23 @@ void Run_tests(){
         delete[] vecA;
         delete[] vecB;
         delete[] copyB;
+        return is_equal(vec_size, vecB, copyB);
+    });
+    test((char*)"DOUBLE CPU OMP4 AND GPU RANDOM PARAMETERS", [](){
+        //init
+        omp_set_num_threads(4);
+        int vec_size = rand()%1800+200;
+        double* vecA = get_rand_vector<double>(vec_size);
+        double* vecB = get_rand_vector<double>(vec_size);
+
+        //run
+        double* copyB = copy_vector(vec_size, vecB);
+        //GPU res in vecB
+        cuda_t_axpy<double>(vec_size, vecA, rand()%20+5, vecB, rand()%20+5, 4.53);
+
+        //CPU res in copyB
+        taxpy<double>(vec_size, vecA, rand()%20+5, copyB, rand()%20+5, 4.53);
+
         return is_equal(vec_size, vecB, copyB);
     });
 }
